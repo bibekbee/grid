5ce6cfc0953860e6626140f7de0bf21665bcc0fa
bibekbee <37708553+bibekbee@users.noreply.github.com>
2023-02-08 21:56:23 +0545

    script change

--- script.js
+++ script.js
@@ -4,9 +4,25 @@ let click = 1;
 let shape = "square";
 let Array = alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
 "AA","BB","CC","DD","EE","FF","GG","HH","II","JJ","KK","LL","MM","NN","OO","PP","QQ","RR","SS","TT","UU","VV","WW","XX","YY","ZZ",];
+let value;
+let android = navigator.userAgent.match(/Android/i)? true : false;
+let Iphone =  navigator.userAgent.match(/iPhone/i)? true : false;
 
 function run() {
     if(click <= 1){
+        if(android || Iphone) {
+            for(let j= 0; j<30; j++){
+                let row = document.createElement("tr");
+               
+                for(let i = 0; i<22; i++) {
+                    let col = document.createElement("td");
+                    col.setAttribute("onclick", `square("${Array[j] + i}")`);
+                    col.setAttribute("id", `${Array[j] + i}` )
+                    row.append(col);
+                }
+                table.append(row);
+                }
+        }else { 
     for(let j= 0; j<40; j++){
     let row = document.createElement("tr");
    
@@ -18,6 +34,7 @@ function run() {
     }
     table.append(row);
     }
+}
 }
 click++;
 }
@@ -35,11 +52,10 @@ function squt() {
 }
 
 function square(a) {
-    console.log(shape)
     let box = document.getElementById(`${a}`);
     let class_name = box.getAttribute("class");
     let confirm = box.getAttribute("class") == null ? true : false;
-    console.log(class_name);
+   
    if(confirm){
     if(shape == "circle"){ box.setAttribute("class", "cir")}
     else if(shape == "triangle"){box.setAttribute("class", "tri")}else{
